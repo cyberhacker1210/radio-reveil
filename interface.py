8bd31b08ed25c1b3c9cf941f2b90dbfd563d8f6e
cyberhacker1210 <leolintello21@gmail.com>
2025-04-20 14:08:42 +0200

    fix full screen bug

--- interface.py
+++ interface.py
@@ -52,7 +52,7 @@ def resize_label(event):
 
 def main_window():
     conf.fenetre = tk.Tk()
-    conf.fenetre.attributes("-fullscreen", True)
+    conf.fenetre.geometry(f"900x700")
     conf.fenetre.title("radio reveil")
     conf.fenetre.configure(bg="#222831")
 
